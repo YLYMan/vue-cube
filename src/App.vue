878beb45b8561e35f99981cfc844422f3859df68
YMan <yly3639@163.com>
2018-11-28 13:56:58 +0800

    tab组件上下联动

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <template>
   <div id="app">
     <v-header :seller="seller"></v-header>
+    <div class="tab-wrapper">
+      <tab></tab>
+    </div>
   </div>
 </template>
 
 <script>
   import VHeader from 'components/v-header/v-header'
+  import Tab from 'components/tab/tab'
   import { getSeller } from 'api' // 配置别名
 
   export default {
@@ -22,17 +26,22 @@
       _getSeller() {
         getSeller().then((seller) => { // 返回一个promise，.then 一定会获取数据(封装后的)
           this.seller = seller
-          console.log(this.seller)
-
         })
       }
     },
     components: {
-      VHeader
+      VHeader,
+      Tab
     }
 
   }
 </script>
 <style lang="stylus">
-
+  #app
+    .tab-wrapper
+      position: fixed
+      top: 136px
+      left: 0
+      right: 0
+      bottom: 0
 </style>
---
+++ src/components/ratings/ratings.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="">I am ratings</div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {
+    name: 'ratings'
+  }
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+
+</style>
